87ebc5efd936b2f16db9d1b87c384d2137c3aa16
xacobovk <xacobovk@gmail.com>
2025-01-23 02:04:35 -0600

    menu theme

--- Assets/js/menu-theme.js
+++ Assets/js/menu-theme.js
@@ -1,5 +1,8 @@
+// Tema oscuro
 const button = document.getElementById('toggle-theme');
 const menuIcon = document.getElementById('menu-icon');
+const nav = document.querySelector('header nav');
+
 button.addEventListener('click', () => {
     const root = document.documentElement;
     const currentTheme = root.getAttribute('data-theme');
@@ -8,13 +11,19 @@ button.addEventListener('click', () => {
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     root.setAttribute('data-theme', newTheme);
 
-    // Cambiar el ícono según el tema
+    // Cambiar los íconos según el tema
     const newMoonIcon = newTheme === 'dark' ? './Assets/image/icons/moon2.svg' : './Assets/image/icons/sun2.svg';
     const newMenuIcon = newTheme === 'dark' ? './Assets/image/icons/menu.svg' : './Assets/image/icons/menu-dark.svg';
 
-    // Actualizar los íconos
+    // Actualizar íconos
     button.setAttribute('src', newMoonIcon);
     menuIcon.setAttribute('src', newMenuIcon);
+
     // Actualizar el texto alternativo del ícono (opcional)
     button.setAttribute('alt', newTheme === 'dark' ? 'modo claro' : 'modo oscuro');
+});
+
+// Menú hamburguesa
+menuIcon.addEventListener('click', () => {
+    nav.classList.toggle('active'); // Mostrar/ocultar el menú
 });
